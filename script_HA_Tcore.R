5218a1e25aac0f389f9387a25ae07979828f97fb
Timointheheat <t.b.van.den.bogaard@vu.nl>
2025-04-17 14:09:00 +0200

    Updated to Newest data (april 2025)
    The new data uploaded by GT needed to be converted into longformat CORE

--- script_HA_Tcore.R
+++ script_HA_Tcore.R
@@ -30,11 +30,12 @@ scoreform_HA <- read_excel(here("data/Scoreform_excel_HA.xlsx"), na = c("NA", ""
 
 # Step 2: create empty longformat ----
 longformat_CORE <- data.frame()
-
+longformat_CORE_rawraw <- data.frame()
 
 
 # Step 3: add each file to longformat_CORE ---- 
 pp_value <- c(1:25)
+                # 25)
 testname <- c("ha1", "ha2", "ha3", "ha4", "ha5", "ha6", "ha7", "ha8", "ha9")
 
 for (participant in pp_value) {
@@ -49,31 +50,36 @@ for (participant in pp_value) {
     temp2 <- NULL
     filtered <- NULL
     filtered2 <- NULL
+    rawraw_CORE <- NULL
     
     ## Create dynamic file names
     filename_chest <- paste("/p", participant, "_", testname[test], "_core_torso_newAlgo", ".csv", sep = "")
     filename_hand <- paste("/p", participant, "_", testname[test], "_core_hand_newAlgo", ".csv", sep = "")
     
     ## CHEST: Check if file exists & do all subsetting
-    if(file.exists(paste(here("data/CORE_rawdata_newAlgorithm/20241220_newnewnew/new_sports_chest_algorithm/"),
+    if(file.exists(paste(here("data/CORE_rawdata_newAlgorithm/April - New Recalculations/"),
                          filename_chest, sep = ""))) {
       
       ### Download data + extract right columns
-      temp <- read.csv(paste(here("data/CORE_rawdata_newAlgorithm/20241220_newnewnew/new_sports_chest_algorithm/"), 
+      temp <- read.csv(paste(here("data/CORE_rawdata_newAlgorithm/April - New Recalculations/"), 
                              filename_chest, sep = "")) %>%
         rename(Date.time = "Timestamp",
                T_core = "old_cbt",
-               T_core_new = "new_cbt",
+               #T_core_new = "new_cbt",
+               T_core_consumer = "consumer_cbt",
                T_skin = "skin_temp",
-               HR = "heart_rate") %>%
+               HR = "heart_rate",
+               consumer_q = "consumer_quality",
+               old_q = "old_quality") %>%
         mutate(Date.time = sub("\\..*", "", Date.time))%>%                          # Delete unexpected ".11" etc
         mutate(date = as.POSIXct(substr(Date.time, 1, 10), format = "%Y-%m-%d"),
                time = as_hms(substr(Date.time, 12, 19))) %>%                        # Convert to useable timestamps
         mutate(T_core_chest = T_core,
-               T_core_chest_new = T_core_new,
+               #T_core_chest_new = T_core_new,
+               T_core_chest_consumer = T_core_consumer,
                T_skin_chest = T_skin,
                HR = as.numeric(HR)) %>%                                   # Convert to Tc
-        dplyr::select(time, T_core_chest, T_core_chest_new, T_skin_chest, HR)
+        dplyr::select(time, T_core_chest, T_core_chest_consumer, T_skin_chest, HR, consumer_q,  old_q)
       
       ### Filter only needed rows by start/ end times
       times <- scoreform_HA[scoreform_HA$pp == participant & scoreform_HA$nmbr_test == test,] %>%
@@ -84,12 +90,21 @@ for (participant in pp_value) {
       filtered <- temp %>%
         filter(time >= times$time_start_45 & time <= times$time_end_ch)
       
+      tocheck_q_chest <- filtered 
       
       ### remove 0-values because they are non-physiological
       filtered <- filtered %>%
         mutate(T_core_chest = ifelse(T_core_chest == 0, NA, T_core_chest),
-               T_core_chest_new = ifelse(T_core_chest_new == 0, NA, T_core_chest_new),
+               T_core_chest_consumer = ifelse(T_core_chest_consumer == 0, NA, T_core_chest_consumer),
                T_skin_chest = ifelse(T_skin_chest == 0, NA, T_skin_chest))
+      
+      
+      ### remove quality score 0-2 because data not good according to GT
+      filtered <- filtered %>%
+        mutate(#T_core_chest = ifelse(consumer_q < 3, NA, T_core_chest),
+               T_core_chest_consumer = ifelse(consumer_q < 3 , NA, T_core_chest_consumer)
+               )
+      
       
       
       
@@ -97,38 +112,42 @@ for (participant in pp_value) {
       averaged_chest <- filtered %>%
         mutate(timestamp = as_hms(round(as.numeric(time) / 60) * 60))%>%              # Get times towards closes minutes
         group_by(timestamp) %>%
-        summarise(across(c(T_core_chest, T_core_chest_new, T_skin_chest, HR), mean, na.rm = TRUE)) %>%            # Calculate mean for each minute
+        summarise(across(c(T_core_chest, T_core_chest_consumer, T_skin_chest, HR), mean, na.rm = TRUE)) %>%            # Calculate mean for each minute
         ungroup() %>%
         mutate(Minutes = as.numeric((timestamp - timestamp[1])/60),
                HR = round(HR)) %>%
-        dplyr::select(Minutes, timestamp, T_core_chest, T_core_chest_new, T_skin_chest, HR) 
+        dplyr::select(Minutes, timestamp, T_core_chest, T_core_chest_consumer, T_skin_chest, HR) 
     } else {
       
       ### If file does not exist, create an empty dataframe
       averaged_chest <- data.frame(Minutes = c(1:166), timestamp = rep(NA, 166), T_core_chest = rep(NA, 166), 
-                         T_core_chest_new = rep(NA, 166), T_skin_chest = rep(NA, 166), HR = rep(NA, 166))
+                         T_core_chest_consumer = rep(NA, 166), T_skin_chest = rep(NA, 166), HR = rep(NA, 166))
     }
     
     ## HAND: Check if file exists & do all subsetting
-    if(file.exists(paste(here("data/CORE_rawdata_newAlgorithm/20241220_newnewnew/wrist_switching_algorithm/"),
+    if(file.exists(paste(here("data/CORE_rawdata_newAlgorithm/April - New Recalculations/"),
                          filename_hand, sep = ""))) {
       
       ### Download data + extract right columns
-      temp2 <- read.csv(paste(here("data/CORE_rawdata_newAlgorithm/20241220_newnewnew/wrist_switching_algorithm/"), 
+      temp2 <- read.csv(paste(here("data/CORE_rawdata_newAlgorithm/April - New Recalculations/"), 
                              filename_hand, sep = "")) %>%
         rename(Date.time = "Timestamp",
                T_core = "old_cbt",
-               T_core_new = "new_cbt",
+               #T_core_new = "new_cbt",
+               T_core_consumer = "consumer_cbt",
                T_skin = "skin_temp",
-               HR = "heart_rate") %>%
+               HR = "heart_rate",
+               consumer_q = "consumer_quality",
+               old_q = "old_quality") %>%
         mutate(Date.time = sub("\\..*", "", Date.time))%>%                          # Delete unexpected ".11" etc
         mutate(date = as.POSIXct(substr(Date.time, 1, 10), format = "%Y-%m-%d"),
                time = as_hms(substr(Date.time, 12, 19))) %>%                        # Convert to useable timestamps
         mutate(T_core_hand = T_core,
-               T_core_hand_new = T_core_new,
+               #T_core_hand_new = T_core_new,
+               T_core_hand_consumer = T_core_consumer,
                T_skin_hand = T_skin,
                HR = as.numeric(HR)) %>%                                   # Convert to Tc
-        dplyr::select(time, T_core_hand, T_core_hand_new, T_skin_hand, HR)
+        dplyr::select(time, T_core_hand, T_core_hand_consumer, T_skin_hand, HR,consumer_q, old_q)
       
       
       ### Filter only needed rows by start/ end times
@@ -140,19 +159,37 @@ for (participant in pp_value) {
       filtered2 <- temp2%>%
         filter(time >= times2$time_start_45 & time <= times2$time_end_ch)
       
+      
+      tocheck_q_hand <- filtered2
+      
+      
+      ### remove 0-values because they are non-physiological
+      filtered2 <- filtered2 %>%
+        mutate(T_core_hand = ifelse(T_core_hand == 0, NA, T_core_hand),
+               T_core_hand_consumer = ifelse(T_core_hand_consumer == 0, NA, T_core_hand_consumer),
+               T_skin_hand = ifelse(T_skin_hand == 0, NA, T_skin_hand))
+      
+      
+      ### remove quality score 0-2 because data not good according to GT
+      filtered2 <- filtered2 %>%
+        mutate(#T_core_hand = ifelse(consumer_q < 3, NA, T_core_hand),
+               T_core_hand_consumer = ifelse(consumer_q < 3 , NA, T_core_hand_consumer),
+        )
+      
+      
       ### average over 1 minute
       averaged_hand <- filtered2 %>%
         mutate(timestamp = as_hms(round(as.numeric(time) / 60) * 60))%>%              # Get times towards closes minutes
         group_by(timestamp) %>%
-        summarise(across(c(T_core_hand, T_core_hand_new, T_skin_hand), mean, na.rm = TRUE)) %>%            # Calculate mean for each minute
+        summarise(across(c(T_core_hand, T_core_hand_consumer, T_skin_hand), mean, na.rm = TRUE)) %>%            # Calculate mean for each minute
         ungroup() %>%
         mutate(Minutes = as.numeric((timestamp - timestamp[1])/60)) %>%
-        dplyr::select(Minutes, timestamp, T_core_hand, T_core_hand_new, T_skin_hand) 
+        dplyr::select(Minutes, timestamp, T_core_hand, T_core_hand_consumer, T_skin_hand) 
     } else {
       
       ### If file does not exist, create an empty dataframe
       averaged_hand <- data.frame(Minutes = c(1:166), timestamp = rep(NA, 166), T_core_hand = rep(NA, 166), 
-                                  T_core_hand_new = rep(NA, 166), T_skin_hand = rep(NA, 166))
+                                  T_core_hand_consumer = rep(NA, 166), T_skin_hand = rep(NA, 166))
       
     }
     
@@ -163,8 +200,18 @@ for (participant in pp_value) {
              ha = test,
              HR_core = HR,
              timestamp = timestamp.x) %>%
-      dplyr::select(pp, ha, Minutes, timestamp, T_core_chest, T_core_chest_new, T_skin_chest,
-                    T_core_hand, T_core_hand_new, T_skin_hand, HR_core)
+      dplyr::select(pp, ha, Minutes, timestamp, T_core_chest, T_core_chest_consumer, T_skin_chest,
+                    T_core_hand, T_core_hand_consumer, T_skin_hand, HR_core)
+    
+    # rawraw_CORE <- tocheck_q_chest %>%
+    #   left_join(tocheck_q_hand, by = "time") %>%
+    #   mutate(pp = participant, 
+    #          ha = test,
+    #          consumer_q_chest = consumer_q.x,
+    #          consumer_q_hand = consumer_q.y)%>%
+    # dplyr::select(pp, ha, time, consumer_q_chest, consumer_q_hand)
+    
+    #longformat_CORE_rawraw <- rbind(longformat_CORE_rawraw, rawraw_CORE)
     
     longformat_CORE <- rbind(longformat_CORE, averaged_CORE)
     
@@ -177,4 +224,4 @@ for (participant in pp_value) {
 # Step 4: save as excel----
 write.xlsx(longformat_CORE,
            file = file.path(paste0(here("data/data_output"), 
-                                   "/HA_longformat_CORE_31032025.xlsx")))
+                                   "/HA_longformat_CORE_17042025.xlsx")))
